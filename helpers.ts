93cd8c5a22365e71ff441d67dcad85fce853715d
TryingToCodeLikeThem <Aeagan0507@gmail.com>
2025-08-16 22:10:22 -0700

    there were input errors I had to fix

--- helpers.ts
+++ helpers.ts
@@ -1,7 +1,9 @@
 //% blockNamespace=images
 namespace helpers {
-    //% blockId="prompt_for_text" block="prompt for text"
-    export declare function _promptForText(): void;
+    //% blockId="prompt_for_text" block="prompt for text $maxLength $numbersOnly"
+    //% maxLength.min=1
+    //% maxLength.max=24
+    export declare function _promptForText(maxLength: number, numbersOnly: number): void;
       //% blockId="get_text_prompt_string" block="get text prompt string"
     export declare function _getTextPromptString(): string;
        //% blockId="get_text_prompt_selection_start" block="get text prompt selection start"
@@ -9,3 +11,4 @@ namespace helpers {
       //% blockId="get_text_prompt_selection_end" block="get text prompt selection end"
     export declare function _getTextPromptSelectionEnd(): number;
 }
+
